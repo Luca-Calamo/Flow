c87ea8632bdc983f2ab321efcda175a1785bd151
luca_calamo <lucacalamo13@gmail.com>
2025-04-28 13:33:00 -0700

    created pages folder

---
+++ app/app/pages/wardrobe.js
@@ -0,0 +1,1 @@
+import WardrobeCardHome from './wardrobe/components/wardrobeCardHome';
--- app/app/wardrobe/components/wardrobeCardHome.js
+++ app/app/wardrobe/components/wardrobeCardHome.js
@@ -2,9 +2,9 @@ import styles from './wardrobeCardHome.module.css';
 export default function WardrobeCardHome() {
     return (
         <div className={styles.cardHome}>
-            <div class={styles.cardHome}>
-                <h2 class={styles.title}> </h2>
-                <img class={styles.img} />
+            <div className={styles.cardHome}>
+                <h2 className={styles.title}> </h2>
+                <img className={styles.img} />
             </div>
         </div>
     );

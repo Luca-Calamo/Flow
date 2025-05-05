b34936f60933f6a4b57f0d69384c1b2c9fbf64a6
emmpup <emisola@my.bcit.ca>
2025-05-05 08:50:28 -0700

    add top bar component

--- app/app/page.js
+++ app/app/page.js
@@ -1,12 +1,24 @@
+import Top_Bar from "@/global-components/top_bar/top_bar";
+
 export default function Home() {
     return (
-        <div className='flex items-center justify-center min-h-screen bg-white'>
-            <a
-                href='/camera'
-                className='px-6 py-3 text-white bg-blue-500 rounded hover:bg-blue-600 transition'
-            >
-                Go to Camera
-            </a>
+        <div className='flex flex-col items-center min-h-screen bg-white'>
+            <Top_Bar
+                title='Title'
+                hasBar={true}
+                leftBtn='Wheel'
+                rightBtn='Canvas'
+                activeBtn='left'
+                hasIcon={true}
+            />
+            <div className='flex-1 flex items-center justify-center w-full'>
+                <a
+                    href='/camera'
+                    className='px-6 py-3 text-white bg-blue-500 rounded hover:bg-blue-600 transition'
+                >
+                    Go to Camera
+                </a>
+            </div>
         </div>
     );
 }

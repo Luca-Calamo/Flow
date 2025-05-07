b4262e411abe863f11063fa51e01c528c20d1429
luca_calamo <lucacalamo13@gmail.com>
2025-05-07 16:10:55 -0700

    created warHome cards

---
+++ app/app/wardrobeHome/page.js
@@ -0,0 +1,13 @@
+import WardrobeCard from '../components/wardrobeHome-comp/warHomeCard';
+
+export default function WarCard() {
+    return (
+        <div className='container'>
+            <img className='img' src='/images/outfit01.png'></img>
+            <img className='img' src='/images/outfit2.png'></img>
+            <img className='img' src='/images/outfit03.png'></img>
+            <img className='img' src='/images/outfit04.png'></img>
+            <img className='img' src='/images/outfit05.png'></img>
+        </div>
+    );
+}

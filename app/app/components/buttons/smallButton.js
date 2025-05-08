878242f8ef055d2f44c522738b8f416d24f8aa2b
luca_calamo <lucacalamo13@gmail.com>
2025-05-07 17:29:57 -0700

    im gunna cry i havent been using componenets. made buttons

---
+++ app/app/components/buttons/smallButton.js
@@ -0,0 +1,9 @@
+import './css/styles.css';
+
+export default function SmallButton() {
+    return (
+        <div className={styles.smlBtn}>
+            <p className={styles.smlBtnText}></p>
+        </div>
+    );
+}

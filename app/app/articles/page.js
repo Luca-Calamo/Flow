8c41699826426385ddcdfaacb7eeedb774dacc8b
luca_calamo <lucacalamo13@gmail.com>
2025-05-12 15:29:18 -0700

    working on article page

--- app/app/articles/page.js
+++ app/app/articles/page.js
@@ -1,12 +1,19 @@
 import ArticleCard from '@/app/components/articles/articleCard';
 import WardrobeCard from '@/app/components/wardrobeHome-comp/warHomeCard';
+import styles from '@/app/articles/css/styles.module.css';
 export default function Articles() {
     return (
         <div>
             <WardrobeCard />
 
-            <div>
-                <ArticleCard />
+            <div className='container'>
+                <ArticleCard src='/images/Black-Dress-Shoes.png' />
+                <ArticleCard src='/images/Black-Dress-Pants.png' />
+                <ArticleCard src='' />
+                <ArticleCard src='' />
+                <ArticleCard src='' />
+                <ArticleCard src='' />
+                <ArticleCard src='' />
             </div>
         </div>
     );

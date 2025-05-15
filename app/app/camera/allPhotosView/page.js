a768e4751cc4b52f162264e8e3ca0cf70cbb3bdb
emmpup <emisola@my.bcit.ca>
2025-05-15 13:42:27 -0700

    add allPhotosView page

---
+++ app/app/camera/allPhotosView/page.js
@@ -0,0 +1,11 @@
+"use client";
+
+import TopBar from "@/app/components/global-components/topBar/topBar";
+
+export default function AllPhotosView() {
+    return (
+        <div>
+            <TopBar />
+        </div>
+    );
+}

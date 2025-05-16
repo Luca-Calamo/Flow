ca03ba23633f44fbc8bcc22d6ae1e693aa80166e
luca_calamo <lucacalamo13@gmail.com>
2025-05-16 13:14:16 -0700

    added components to fits page 1

--- app/app/individual-fits/page.js
+++ app/app/individual-fits/page.js
@@ -1,10 +1,23 @@
 import IndividualFits from '@/app/components/individual-fits/IndiFitsCard';
+import TopBar from '@/app/components/global-components/topBar/topBar';
+import Navbar from '@/app/components/global-components/bottom-navbar/navbar';
+import SmallButton from '@/app/components/buttons/smallButton';
 import styles from '@/app/individual-fits/css/styles.module.css';
 
 export default function IndiFits() {
     return (
-        <div className={styles.body}>
-            <IndividualFits />
+        <div className={styles.container}>
+            <TopBar title='Fits' hasIcon={true} />
+            <div className={styles.btncontainer}>
+                <div className={styles.button}>
+                    <SmallButton text='Edit' />
+                </div>
+                <p className={styles.text}>Back to Wardrobe</p>
+            </div>
+            <div className={styles.body}>
+                <IndividualFits src=' /images/Fit1.png' />
+            </div>
+            <Navbar />
         </div>
     );
 }

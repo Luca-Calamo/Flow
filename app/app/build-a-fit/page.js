a64aeebe4a2ea42b48a51d8b05a88f2b1e91f73d
luca_calamo <lucacalamo13@gmail.com>
2025-05-17 17:54:21 -0700

    working on carousel

--- app/app/build-a-fit/page.js
+++ app/app/build-a-fit/page.js
@@ -1,10 +1,13 @@
 import Carousel from '@/app/components/carousel/carousel';
-
+import Navbar from '@/app/components/global-components/bottom-navbar/navbar';
+import TopBar from '@/app/components/global-components/topBar/topBar';
+import styles from '@/app/build-a-fit/css/styles.module.css';
 export default function BuildAFit() {
     return (
-        <main>
-            <h1>My Clothing Carousel</h1>
+        <div className={styles.body}>
+            <TopBar title='Build a Fit' hasIcon={true} initialActive='right' />
             <Carousel />
-        </main>
+            <Navbar />
+        </div>
     );
 }

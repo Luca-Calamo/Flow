1e71c6013a5d94fdbd2cfb6f94e459c517cf4d92
PabloVega7 <jbojorquezvega@my.bcit.ca>
2025-05-19 12:24:30 -0700

    link home icon in navbar to home page.js

--- app/app/page.js
+++ app/app/page.js
@@ -1,14 +1,5 @@
-"use client";
+import HomePage from "./home/page";
 
-import Image from "next/image";
-import Statusbar from "@/app/components/global-components/statusbar/Statusbar";
-import Navbar from "@/app/components/global-components/bottom-navbar/navbar";
-
-export default function SuggestionPage() {
-    return (
-        <div>
-            <Statusbar titleBar={true} title="Fits" showExitButton={false} />
-            <Navbar />
-        </div>
-    );
+export default function Page() {
+  return <HomePage />;
 }
